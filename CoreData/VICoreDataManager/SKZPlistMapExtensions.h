29928ff67b1397091138a31319a3e5c66513e618
John Graziano <jgraziano@skillz.com>
2016-01-31 13:09:06 -0800

    initial core data shim

---
+++ CoreData/VICoreDataManager/SKZPlistMapExtensions.h
@@ -0,0 +1,13 @@
+//
+//  SKZPlistMapExtensions.h
+//  SkillzSDK-iOS
+//
+//  Created by John Graziano on 1/29/16.
+//  Copyright © 2016 Skillz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKZPlistMapExtensions : NSObject
+
+@end
---
+++ CoreData/VICoreDataManager/SKZPlistMapExtensions.m
@@ -0,0 +1,13 @@
+//
+//  SKZPlistMapExtensions.m
+//  SkillzSDK-iOS
+//
+//  Created by John Graziano on 1/29/16.
+//  Copyright © 2016 Skillz. All rights reserved.
+//
+
+#import "SKZPlistMapExtensions.h"
+
+@implementation SKZPlistMapExtensions
+
+@end
